e0d4171136c587f6434709a6b77d161637a16968
Ravi <Rpatel1291@gmail.com>
2018-03-23 16:03:36 -0400

    Updated keys
    updated username/keys for watson and twitter api

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "consumer_key": "WbHJyzTXN8RV5mglTnuv7ZXAj",
-    "consumer_secret": "0NVl3ugpH0KKD8ouuHBzIt1AVAEeGMz98Wzm54JPZgDKIAkDTX",
+    "consumer_key": "1OFcL6eJGM5sF9Xi3zAPL7Nuw",
+    "consumer_secret": "BldG8lYCk7tNIXV1j8ZH9Zhk4BLjtMF6AjqsV8ctsppLUmUCAl",
     "access_token": "834039778911846401-8EPQyVWzF89vFECiyeN7pQAy4K1Jj5l",
     "access_token_secret": "baZnSmhPZIgbqdfL7Rrtj9z9DIHzsodTLjcfo7BqBSQIO"
   }
