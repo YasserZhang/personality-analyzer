f1de8aa07bf207629354f0b97a47d798a6a55810
Ravi <Rpatel1291@gmail.com>
2018-04-17 13:51:34 -0400

    Updates Made by Kandarp

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ const tweetRoutes = require("./tweets");
 const insightRoutes = require("./insights");
 
 const constructorMethod = app => {
-  app.use("/handles", handleRoutes);
-  app.use("/tweets", tweetRoutes);
-  app.use("/insights", insightRoutes);
-
-  app.use("*", (req, res) => {
-    res.sendStatus(404);
-  });
+    app.use("/handles", handleRoutes);
+    app.use("/tweets", tweetRoutes);
+    app.use("/insights", insightRoutes);
+    app.use("*", (req, res) => {
+        res.sendStatus(404);
+    });
 };
 
 module.exports = constructorMethod;
